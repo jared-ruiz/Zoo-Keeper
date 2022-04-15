c842c502822fe56010b051c6fd2243a7c3f346b6
Jared Ruiz <jared707@gmail.com>
2022-04-15 18:42:02 -0500

    Added post route and validations for data.

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const { query } = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+//parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 //testing url for development only: https://peaceful-forest-08175.herokuapp.com/api/animals
@@ -50,6 +59,41 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+
+    );
+
+    //return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
 
@@ -69,6 +113,21 @@ app.get('/api/animals/:id', (req, res) => {
     else {
         //returns not found to end user if search does not exist
         res.send(404);
+    }
+})
+
+app.post('/api/animals', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
+        //add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
 })
 
